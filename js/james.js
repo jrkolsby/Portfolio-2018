b703610973b511ef7d635b6122ac9ccf7a9cc305
James Kolsby <jrkolsby@mac.com>
2018-01-29 01:14:46 -0500

    Initial commit

---
+++ js/james.js
@@ -0,0 +1,26 @@
+
+const observer = lozad('.lazy', {
+    rootMargin: '10px 0px',
+    threshold: 0.1
+});
+
+$(document).ready(function() {
+	var $title = $('h1');
+    var $links = $('button');	
+    var $outer = $('nav');
+
+    $title.data("color", $title.attr("data-color"));
+
+    for (var i = 0; i++; i < $links.length) {
+        var $link = $links[i];
+        $link.data("color", $link.attr("data-color"));
+    }
+
+    $outer.mouseout(function() {
+        $title.css("color", $(this).data("color"));
+    });
+
+    $links.mouseenter(function() {
+        $title.css("color", $(this).data("color"));
+    });
+});
